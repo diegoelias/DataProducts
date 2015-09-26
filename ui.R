a4b650c37d6bd20a56e5c462cca8d8fab97c0b53
diegoelias <diego.uesc@gmail.com>
2015-09-25 22:05:50 -0300

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,22 +1,34 @@
 library(shiny)
-shinyUI(
-pageWithSidebar(
-  headerPanel("Motor Trend Car Road Tests"),
-  sidebarPanel(
-    h4('The data was extracted from the 1974 Motor Trend US magazine, and comprises fuel consumption and 10 aspects of automobile design and performance for 32 automobiles (1973–74 models).'),
-    h4('Select he number of cylinders and /or the maximum Horse Power and we shall show you the corresponding average  Miles/Gallon'),
-    
-	h3("Enter the data"),
-	
-	radioButtons("cyl", "Number of cylinders:",
-             c("4" = "4",
-               "6" = "6",
-               "8" = "8")),
-			   
-	numericInput('horse', 'Horse Power input: ', 140, min = 70, max = 250, step = 10)			   
-	),
-  mainPanel(
-    h4("Miles/(US) gallon"),
-	verbatimTextOutput("mpgId")
-	)
+shinyUI(pageWithSidebar(
+    headerPanel('Developing Data Products'),
+    sidebarPanel(
+        h3('Instructions'),
+        p('Enter the gross horsepower, number of cylinders, and weight of your 
+          car below.  The predicted MPG will be shown to the right.'),
+        h3('Please enter predictors of MPG below.'),
+        numericInput('hp', 'Gross horsepower:', 140, min = 50, max = 330, step = 10), # example of numeric input
+        radioButtons('cyl', 'Number of cylinders:', c('4' = 4, '6' = 6, '8' = 8), selected = '4'), # example of radio button input
+        numericInput('wt', 'Weight (lbs):', 3200, min = 1500, max = 5500, step = 100)
+    ),
+    mainPanel(
+        h6('Course Project by Diego Elias'),
+        h3('Predicted MPG'),
+        h4('You entered:'),
+        verbatimTextOutput("inputValues"),
+        h4('Which resulted in a prediction of:'),
+        verbatimTextOutput("prediction"),
+        h4('MPG relative to cars in mtcars data set'),
+        plotOutput('plots'),
+        h3('Method'),
+        p('I fit a linear model to the mtcars dataset, modeling the effect of 
+          horsepower, number of cylinders, and weight on the mpg.  Since the 
+          only valid possibilities for number of cylinders in the dataset were 
+          4, 6, and 8, I limited the choice using radio buttons.  For the weight,
+          reactive() is used to convert the user input weight into the units 
+          used by the model, lb/1000.  Finally, a pre-set function using the 
+          linear model is used to predict the mpg based on the three variables 
+          input by the user.'),
+        h5('References'),
+        p('Henderson and Velleman (1981), Building multiple regression models interactively. Biometrics, 37, 391-411.')
+    )
 ))
